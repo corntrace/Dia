717f763b83e26920d297a4e3f0e5ee14dc3f372a
robert gleeson <robertgleeson@robert-gleesons-iMac.local>
2010-02-02 10:36:18 +0000

    Modify examples/client.rb

--- client.rb
+++
@@ -1,7 +0,0 @@
-require 'rubygems'
-require 'lib/dia'
-
-sandbox = Dia::SandBox.new("/Applications/Firefox.app/Contents/MacOS/firefox-bin", Dia::Profiles::NO_INTERNET)
-sandbox.run
-
-puts "Running firefox with a PID of #{sandbox.pid}, using the #{sandbox.profile} profile"
