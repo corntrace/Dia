53c821ae107daa338b0949a9214e7d45a0a861c7
robert gleeson <robertgleeson@hope.(none)>
2010-02-10 01:35:35 +0000

    Add example for run_block

---
+++ examples/sandboxed_block.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'open-uri'
+require 'net/http'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'dia')
+
+sandbox = Dia::SandBox.new('', Dia::Profiles::NO_OS_SERVICES)
+sandbox.run_block do
+  open("http://www.google.com").read
+end
