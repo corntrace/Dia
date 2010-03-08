31f54c5bd58377d07cd3588f384668ec488e34d3
robert gleeson <robertgleeson@hope.(none)>
2010-03-08 23:53:28 +0000

    Remove examples/ (outdated examples) ..

--- examples/client.rb
+++
@@ -1,5 +0,0 @@
-require 'rubygems'
-require 'lib/dia'
-
-Dia::Sandbox.new(Dia::Profiles::NO_FILESYSTEM_WRITE, "echo 'foo' > foo.txt").run
-Dia::Sandbox.new(Dia::Profiles::NO_FILESYSTEM_WRITE, "echo 'foo' > foo.txt").run
--- examples/sandboxed_block.rb
+++
@@ -1,9 +0,0 @@
-require 'rubygems'
-require 'open-uri'
-require 'net/http'
-require File.join(File.dirname(__FILE__), '..', 'lib', 'dia')
-
-sandbox = Dia::SandBox.new(Dia::Profiles::NO_OS_SERVICES)
-sandbox.run_with_block do
-  open("http://www.google.com").read
-end
