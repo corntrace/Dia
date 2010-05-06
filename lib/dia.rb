11845f75bff220e6c5b04976fea04bc02efc84a2
robert gleeson <robertgleeson@hope.(none)>
2010-05-07 00:52:20 +0100

    Release 1.5.pre.2

--- lib/dia.rb
+++ lib/dia.rb
@@ -5,7 +5,7 @@ require File.join(File.dirname(__FILE__), 'dia/commonapi.rb')
 require File.join(File.dirname(__FILE__), 'dia/sandbox.rb')
 
 module Dia
-  VERSION = '1.5.pre'
+  VERSION = '1.5.pre.2'
   class SandboxException < StandardError; end
 end
 
