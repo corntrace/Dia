4472b12e5ef665e45df6cf9b902d2b7c2281448c
robert gleeson <robertgleeson@hope.(none)>
2010-05-07 00:15:55 +0100

    Add greater test coverage for Dia::Sandbox#exception()

--- test/suite/exceptions_text.rb
+++ test/suite/exceptions_text.rb
@@ -1,11 +1,31 @@
 BareTest.suite('Exceptions', :tags => [ :exception ]) do
   assert('Dia::SandboxException is raised if a call to sandbox_init() fails') do
     sandbox = Dia::Sandbox.new(Dia::Profiles::NO_OS_SERVICES) do 
-                # ...
-              end
+      # ...
+    end
     sandbox.instance_variable_set("@profile", "i am going to break")
     sandbox.run
     sleep(0.1)
     equal(Dia::SandboxException, sandbox.exception().class)
   end
+
+  assert('Dia::Sandbox#exception() returns nil if called before ' \
+         'Dia::Sandbox#run()') do
+    sandbox = Dia::Sandbox.new(Dia::Profiles::NO_OS_SERVICES) do
+                # ...
+              end
+    equal(nil, sandbox.exception())
+  end
+
+  assert('Dia::Sandbox#exception() returns nil after a second call') do
+    sandbox = Dia::Sandbox.new(Dia::Profiles::NO_OS_SERVICES) do 
+      raise()
+    end
+  
+    sandbox.run()
+    sleep(0.1)
+    equal(RuntimeError, sandbox.exception().class)
+    equal(nil, sandbox.exception())
+  end
+
 end
