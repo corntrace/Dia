17406fd5f48dc5807f9049485effc4faef2ddc2e
robert gleeson <robertgleeson@hope.(none)>
2010-05-07 00:10:20 +0100

    Add test coverage for the failure of sandbox_init()

---
+++ test/suite/exceptions_text.rb
@@ -0,0 +1,11 @@
+BareTest.suite('Exceptions', :tags => [ :exception ]) do
+  assert('Dia::SandboxException is raised if a call to sandbox_init() fails') do
+    sandbox = Dia::Sandbox.new(Dia::Profiles::NO_OS_SERVICES) do 
+                # ...
+              end
+    sandbox.instance_variable_set("@profile", "i am going to break")
+    sandbox.run
+    sleep(0.1)
+    equal(Dia::SandboxException, sandbox.exception().class)
+  end
+end
