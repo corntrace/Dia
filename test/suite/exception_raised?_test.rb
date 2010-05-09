e17e1e245c7255b4edf12dc3cff08c34d7d89998
robert gleeson <robertgleeson@robert-gleesons-iMac.local>
2010-05-10 00:07:55 +0100

    Add suite for Dia::Sandbox#exception_raised?()

---
+++ test/suite/exception_raised?_test.rb
@@ -0,0 +1,22 @@
+BareTest.suite('Dia::Sandbox#exception_raised?()', 
+               :tags => [ :exception_raised] ) do
+  assert('#exception_raised?() returns false when no exception has been ' \
+         'raised') do
+    sandbox = Dia::Sandbox.new(Dia::Profiles::NO_OS_SERVICES) do
+      # ...
+    end 
+    sandbox.run()
+    sleep(0.1)
+    equal(false, sandbox.exception_raised?())
+  end
+
+  assert('#exception_raised?() returns true when an exception has been ' \
+         'raised') do
+    sandbox = Dia::Sandbox.new(Dia::Profiles::NO_OS_SERVICES) do
+      raise(StandardError, 'Exception')
+    end
+    sandbox.run()
+    sleep(0.1)
+    equal(true, sandbox.exception_raised?())
+  end
+end
