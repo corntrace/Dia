203e1af5d695454ae8108447424c182fbd7e7eae
robert gleeson <robertgleeson@hope.(none)>
2010-05-21 06:24:56 +0100

    Implement Dia::FFI

---
+++ lib/dia/ffi.rb
@@ -0,0 +1,8 @@
+module Dia
+  module FFI
+    extend(FFI::Library)
+    ffi_lib(%w(system))
+    attach_function(:sandbox_init, [ :pointer, :uint64, :pointer ], :int)
+    attach_function(:sandbox_free_error, [ :pointer ], :void)  
+  end
+end
