6625f642df6b85c646a73bed66869164a7778cb7
robert gleeson <robertgleeson@hope.(none)>
2010-05-27 13:45:44 +0100

    Remove 1.0 compatiability layer…

--- lib/dia/1.0.rb
+++
@@ -1,1 +0,0 @@
-require(File.expand_path('1.0/dia', File.dirname(__FILE__)))
--- lib/dia/1.0/commonapi.rb
+++
@@ -1,7 +0,0 @@
-module Dia
-  module CommonAPI
-    extend FFI::Library
-    ffi_lib(%w(sandbox system libSystem.B.dylib))
-    attach_function :sandbox_init, [ :pointer, :uint64, :pointer ], :int
-  end
-end
