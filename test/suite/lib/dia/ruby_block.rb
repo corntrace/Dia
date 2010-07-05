1386aa31a36f391fb7bcf45006df380c8527cba2
robgleeson <rob@flowof.info>
2010-07-06 00:35:05 +0100

    RubyBlock full test coverage.

--- test/suite/lib/dia/ruby_block.rb
+++ test/suite/lib/dia/ruby_block.rb
@@ -1,10 +1,38 @@
 suite('Dia::RubyBlock') do
+
+  suite('#rescue_exception?()') do
+    exercise('@rescue is set to false, ' \
+             '#rescue_exception?() returns false') do
+      sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_OS_SERVICES) do
+        #...
+      end
+      @result = sandbox.rescue_exception?()
+    end
+
+    verify(nil) do
+      @result == false      
+    end
+
+    exercise('@rescue is set to true, ' \
+             '#rescue_exception?() returns true') do
+      sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_OS_SERVICES) do
+      end
+      sandbox.rescue_exception = true
+      @result = sandbox.rescue_exception?()
+    end
+
+    verify(nil) do
+      @result == true
+    end
+  end
+
   suite('#exception()') do 
     setup do 
       @result = nil
     end
 
-    exercise('@rescue is set to false, #run() is executed, ' \
+    exercise('@rescue is set to false, ' \
+             '#run() called, ' \
              '#exception() returns nil to the parent process.') do
       sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_OS_SERVICES) do
         begin
@@ -21,9 +49,9 @@ suite('Dia::RubyBlock') do
     end
 
     exercise('@rescue is set to true, ' \
-             '#run() is executed, ' \
-             'an exception is raised in sandbox, ' \
-             '#exception() returns the raised exception ' \
+             '#run() called, ' \
+             'exception raised, ' \
+             '#exception() returns the exception ' \
              'to the parent process.') do
       sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_OS_SERVICES) do
         raise()
@@ -36,6 +64,26 @@ suite('Dia::RubyBlock') do
     verify(nil) do
       @result == RuntimeError
     end
+
+    exercise('@rescue is set to true, ' \
+             '#run() called, ' \
+             'exception raised, ' \
+             '@rescue is set to false, ' \
+             '#exception() returns raised exception to parent process') do
+
+      sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_INTERNET) do
+        raise()
+      end
+      sandbox.rescue_exception = true
+      sandbox.run()
+      sandbox.rescue_exception = false
+      @result = sandbox.exception().class()
+    end
+
+    verify(nil) do 
+      @result == RuntimeError
+    end
+
   end
 
   suite('#run()') do
@@ -150,7 +198,125 @@ suite('Dia::RubyBlock') do
     verify(nil) do
       @result == 'true'
     end
-
   end
+
+    suite('#run_nonblock()') do
+    setup do
+      @result          = nil
+      @reader, @writer = IO.pipe
+    end
+
+    exercise('Confirm the profile Dia::Profiles::NO_INTERNET is creating a ' \
+             'working sandbox environment.') do      
+      sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_INTERNET) do
+        begin
+          @reader.close
+          TCPSocket.open('http://www.google.com', 80)
+          @writer.write('false')
+        rescue SocketError, SystemCallError => e
+          @writer.write('true')
+        ensure
+          @writer.close()
+        end
+      end
+
+      sandbox.run_nonblock()
+      sleep(1)
+
+      @writer.close()
+      @result = @reader.gets()
+      @reader.close()
+    end
+
+    verify(nil) do
+      @result == 'true'
+    end
+
+    exercise('Confirm the profile Dia::Profiles::NO_FILESYSTEM_WRITE is ' \
+             'creating a working sandbox environment.') do      
+      sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_FILESYSTEM_WRITE) do
+        begin
+          @reader.close
+          File.open('/tmp/foo.txt', 'w') { |f| f.puts('fail') }
+          @writer.write('false')
+        rescue SocketError, SystemCallError => e
+          @writer.write('true')
+        ensure 
+          @writer.close()
+        end
+      end
+      
+
+      sandbox.run_nonblock()
+      sleep(1)
+
+      @writer.close()
+      @result = @reader.gets()
+      @reader.close()
+    end
+
+    verify(nil) do
+      @result == 'true'
+    end
+
+    exercise('Confirm the profile ' \
+             'Dia::Profiles::NO_FILESYSTEM_WRITE_EXCEPT_TMP ' \
+             'is creating a working sandbox environment. ') do      
+      sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_FILESYSTEM_WRITE_EXCEPT_TMP) do
+        begin
+          @reader.close
+          out = Time.now.to_s
+          File.open('/tmp/%s.dia_test' % [ out ] , 'w') { |f| f.puts('success') }
+          File.open(File.join(ENV['HOME'], 'fail.txt')) { |f| f.puts('fail') }
+          @writer.write('false')
+        rescue SocketError, SystemCallError => e
+          if File.exists?('/tmp/%s.dia_test' % [ out ])
+            @writer.write('true')
+          else
+            @writer.write('false')
+          end
+        ensure 
+          @writer.close()
+        end
+      end
+      
+      sandbox.run_nonblock()
+      sleep(1)
+
+      @writer.close()
+      @result = @reader.gets()
+      @reader.close()
+    end
+
+    verify(nil) do
+      @result == 'true'
+    end
+
+    exercise('Confirm the profile ' \
+             'Dia::Profiles::NO_NETWORKING ' \
+             'is creating a working sandbox environment') do
+      sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_NETWORKING) do  
+        begin
+          @reader.close()
+          TCPSocket.open('http://www.youtube.com', 80)
+          @writer.write('false')
+        rescue SocketError => e
+          @writer.write('true')
+        end
+      end
+      
+      sandbox.run_nonblock()
+      sleep(1)
+      
+      @writer.close()
+      @result = @reader.gets()
+      @reader.close()    
+    end
+
+    verify(nil) do
+      @result == 'true'
+    end
+  end
+
 end
 
