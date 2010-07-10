e593ae4708ee4d89d33e1b7d49e197a2a1d2f7e8
robgleeson <rob@flowof.info>
2010-07-10 06:12:14 +0100

    Bump to version 2.0.0.pre

--- lib/dia.rb
+++ lib/dia.rb
@@ -6,5 +6,5 @@ require(File.expand_path('dia/profiles'       , File.dirname(__FILE__)))
 require(File.expand_path('dia/ruby_block'     , File.dirname(__FILE__)))
 require(File.expand_path('dia/exceptions'     , File.dirname(__FILE__)))
 module Dia
-  VERSION = '2.0'
+  VERSION = '2.0.0.pre'
 end
