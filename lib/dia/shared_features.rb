corntrace/Dia

commit e7684df94b4598b9aa8d1bd2c9b8767c1d4ea23b

Fix a possible scenario where a zombie could be accumulated after sending 'SIGKILL' to a child process.

lib/dia/shared_features.rb
@@ -29,6 +29,7 @@ module Dia
     #                           been called yet. 
     def terminate()
       Process.kill('SIGKILL', @pid) unless @pid.nil?
+      Process.detach(@pid) if running? # precaution against accumulating zombies. 
     end
     
     # This method will tell you whether or not your sandbox is still running.

commit 72efd32ff41207adbd0e0d6dc4695f435cab14b7

Return the value from Process.kill() in #terminate. A bug introduced in e7684df94b4598b9aa8d1bd2c9b8767c1d4ea23b.

lib/dia/shared_features.rb
@@ -8,8 +8,7 @@ module Dia
     # @return [Fixnum, nil] Returns the exit status of your sandbox as a 
     #                       Fixnum.  
     #                       Returns nil if #run or #run_nonblock has not
-    #                       been called yet, or if the process stopped
-    #                       abnormally(ie: through {#terminate}).
+    #                       been called yet.
     # @since 1.5
     def exit_status()
       unless @exit_status.nil?
@@ -28,8 +27,10 @@ module Dia
     #                           Returns nil if #run or #run_nonblock has not 
     #                           been called yet. 
     def terminate()
-      Process.kill('SIGKILL', @pid) unless @pid.nil?
-      Process.detach(@pid) if running? # precaution against accumulating zombies. 
+      ret = Process.kill('SIGKILL', @pid) unless @pid.nil?
+      # precaution against the collection of zombie processes.
+      @exit_status = Process.detach(@pid) if running? 
+      ret
     end
     
     # This method will tell you whether or not your sandbox is still running.