1222758cc0b33b01bb8a973834dc8eccabd089fd
robgleeson <rob@flowof.info>
2010-07-13 06:40:49 +0100

    Improve the test suite for Dia::SharedFeatures#exit_status.
    I applied some general cleaning to the suite regarding #exit_status, and also
    applied a baretest tag to the suite.
    
    As of now, selectors(or tags) seem to be broken on baretest 0.5.0.pre but will come
    in useful in the future.
    
    I've also refactored the suite to take advantage of exercise/verify, something I haven't been
    doing but I'm changing that(slowly).

--- test/suite/lib/dia/shared_features.rb
+++ test/suite/lib/dia/shared_features.rb
@@ -2,35 +2,28 @@ suite('Dia::SharedFeatures') do
 
   suite('RubyBlock') do
 
-    suite('#exit_status()') do
+    suite('#exit_status', :tags => [ :exit_status ] ) do
+
       setup do
         @result = nil
       end
 
-      exercise('#run() called, ' \
-               'exits with status of 10, ' \
-               '#exit_status() returns 10') do
-        sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_INTERNET) do
-          exit(10)
-        end
-        sandbox.run()
-        @result = sandbox.exit_status()
+      exercise('#run is called, exits process with status of 10. ') do
+        sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_INTERNET) { exit(10) }
+        sandbox.run
+        @result = sandbox.exit_status
       end
 
-      verify(nil) do
+      verify('#exit_status returns 10') do
         @result == 10
       end
 
-      exercise('When #run() or #run_nonblock() has not been called, ' \
-               '#exit_status() returns nil') do
-
-        sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_INTERNET) do
-          # ..
-        end
-        @result = sandbox.exit_status()
+      exercise('Neither #run or #run_nonblock has been called. ') do
+        sandbox = Dia::RubyBlock.new(Dia::Profiles::NO_INTERNET) { }
+        @result = sandbox.exit_status
       end
 
-      verify(nil) do
+      verify('#exit_status returns nil') do
         @result == nil
       end
 
